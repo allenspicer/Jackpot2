348487cf8c778bb42b45e506038f7b8318dc45cb
Allen Spicer <allenspicer@gmail.com>
2016-05-08 17:56:54 -0400

    Initial Commit

---
+++ Jackpot2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jackpot2
+//
+//  Created by Allen Spicer on 5/8/16.
+//  Copyright © 2016 Allen Spicer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jackpot2/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  Jackpot2
+//
+//  Created by Allen Spicer on 5/8/16.
+//  Copyright © 2016 Allen Spicer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface MasterViewController : UITableViewController
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+
+@end
+
